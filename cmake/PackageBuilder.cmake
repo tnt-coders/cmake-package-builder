608354971d160b275aa0a7900b3c4d120bfe2a50
tjwrona <tjwrona1992@gmail.com>
2026-01-28 23:29:51 -0500

    Changed package name and greatly clarified how things work. README still needs work.

--- cmake/PackageBuilder.cmake
+++ cmake/PackageBuilder.cmake
@@ -3,21 +3,29 @@ include_guard(GLOBAL)
 include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
 
-function(_core_get_project_version)
+function(_package_check_initialized)
+    get_property(package_initialized GLOBAL PROPERTY PACKAGE_INITALIZED)
+    if(NOT package_initialized)
+        message(FATAL_ERROR "package_init() must be called before all other PackageBuilder functions.")
+    endif()
+endfunction()
 
-    # If project version is already defined, return
-    if(PROJECT_VERSION)
-        return()
+function(package_init)
+
+    # Check if Conan exists. If it does, export by default
+    find_program(CONAN_EXECUTABLE conan)
+    if(CONAN_EXECUTABLE)
+        option(CONAN_EXPORT "Export a Conan package" ON)
     endif()
 
-    # Get the version information from Git
-    find_package(Git QUIET)
-    if(git_FOUND)
+    # If no version is specified, get it from Git
+    if(NOT PROJECT_VERSION)
+        find_package(Git REQUIRED)
 
         # Use "git describe" to get version information from Git
         execute_process(
                 COMMAND ${GIT_EXECUTABLE} describe --dirty --long --match=v* --tags
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                 RESULT_VARIABLE git_result
                 OUTPUT_VARIABLE git_output OUTPUT_STRIP_TRAILING_WHITESPACE
                 ERROR_VARIABLE git_error ERROR_STRIP_TRAILING_WHITESPACE)
@@ -50,7 +58,7 @@ function(_core_get_project_version)
         # Use "git log" to get the current commit hash from Git
         execute_process(
                 COMMAND ${GIT_EXECUTABLE} log -1 --pretty=format:%H
-                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                 RESULT_VARIABLE git_result
                 OUTPUT_VARIABLE git_output OUTPUT_STRIP_TRAILING_WHITESPACE
                 ERROR_VARIABLE git_error ERROR_STRIP_TRAILING_WHITESPACE)
@@ -78,38 +86,45 @@ function(_core_get_project_version)
         set(${PROJECT_NAME}_VERSION_TWEAK ${version_tweak} PARENT_SCOPE)
         set(${PROJECT_NAME}_VERSION_IS_DIRTY ${version_is_dirty} PARENT_SCOPE)
         set(${PROJECT_NAME}_VERSION_HASH ${version_git_hash} PARENT_SCOPE)
-
     endif()
 
     if(NOT PROJECT_VERSION)
-        set(PROJECT_VERSION "0.0.0" PARENT_SCOPE)
-        set(PROJECT_VERSION_MAJOR "0" PARENT_SCOPE)
-        set(PROJECT_VERSION_MINOR "0" PARENT_SCOPE)
-        set(PROJECT_VERSION_PATCH "0" PARENT_SCOPE)
-        message(WARNING "Project version could not be determined."
-            "Defaulting to 0.0.0."
-            "Either specify the project version manually or add a Git tag(v<major>.<minor>.<patch>) to override.")
+        message(FATAL_ERROR "Package version could not be determined.")
     endif()
+
+    set(GLOBAL PROPERTY PACKAGE_INITALIZED TRUE)
 endfunction()
 
-function(core_install)
-    set(options)
-    set(one_value_args NAMESPACE)
-    set(multi_value_args TARGETS)
-    cmake_parse_arguments(args "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+function(package_add_executable)
+    _package_check_initialized()
 
-    _core_get_project_version()
+    add_executable(${ARGN})
+    set_property(GLOBAL APPEND PROPERTY ${PROJECT_NAME}_TARGETS ${ARGV0})
+endfunction()
+
+function(package_add_library)
+    _package_check_initialized()
+
+    add_library(${ARGN})
+    set_property(GLOBAL APPEND PROPERTY ${PROJECT_NAME}_TARGETS ${ARGV0})
+    add_library(${PROJECT_NAME}::${ARGV0} ALIAS ${ARGV0})
+endfunction()
+
+function(package_install)
+    _package_check_initialized()
+
+    get_property(targets GLOBAL PROPERTY ${PROJECT_NAME}_TARGETS)
 
     # Set the install destination and namespace
-    set(install_destination "lib/cmake/${PROJECT_NAME}")
+    set(install_destination "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
 
-    if(args_TARGETS)
+    if(targets)
         # Create an export package of the targets
         # Use GNUInstallDirs and COMPONENTS
         # See "Deep CMake for Library Authors" https://www.youtube.com/watch?v=m0DwB4OvDXk
         # TODO: Implement COMPONENTS
         install(
-                TARGETS ${args_TARGETS}
+                TARGETS ${targets}
                 EXPORT ${PROJECT_NAME}Targets
                 ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
                 #COMPONENT Development
@@ -127,22 +142,19 @@ function(core_install)
                 FILE ${PROJECT_NAME}Targets.cmake
                 NAMESPACE ${args_NAMESPACE}
                 DESTINATION ${install_destination})
-
-        # Store installed targets for later use by core_generate_package_config
-        set(${PROJECT_NAME}_INSTALLED_TARGETS ${args_TARGETS} PARENT_SCOPE)
     endif()
-
-    # Install public CMake modules for the project
-    install(
-            DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/cmake/
-            DESTINATION ${install_destination}
-            FILES_MATCHING PATTERN "*.cmake")
 
     # Install public header files for the project
     install(
-            DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/include/
+            DIRECTORY ${PROJECT_SOURCE_DIR}/include/
             DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
             FILES_MATCHING PATTERN "*.h*")
+
+    # Install public CMake modules for the project
+    install(
+            DIRECTORY ${PROJECT_SOURCE_DIR}/cmake/
+            DESTINATION ${install_destination}
+            FILES_MATCHING PATTERN "*.cmake")
 
     # Generate a package configuration file
     set(config_content "@PACKAGE_INIT@\n")
@@ -150,7 +162,7 @@ function(core_install)
     string(APPEND config_content "\nlist(APPEND CMAKE_MODULE_PATH \"\${CMAKE_CURRENT_LIST_DIR}\")\n")
 
     # Include targets file if we have targets installed
-    if(${PROJECT_NAME}_INSTALLED_TARGETS)
+    if(${targets})
         string(APPEND config_content "\ninclude(\${CMAKE_CURRENT_LIST_DIR}/${PROJECT_NAME}Targets.cmake)")
     endif()
 
@@ -163,28 +175,34 @@ function(core_install)
     # Gather files to be installed
     list(APPEND install_files ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake)
 
-    # If the package has a version specified, generate a package version file
-    if(PROJECT_VERSION)
-        write_basic_package_version_file(${PROJECT_NAME}ConfigVersion.cmake
-                VERSION ${PROJECT_VERSION}
-                COMPATIBILITY SameMajorVersion)
+    # Generate a package version file
+    write_basic_package_version_file(${PROJECT_NAME}ConfigVersion.cmake
+            VERSION ${PROJECT_VERSION}
+            COMPATIBILITY SameMajorVersion)
 
-        list(APPEND install_files ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake)
-    endif()
+    list(APPEND install_files ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake)
 
     # Install config files for the project
     install(
             FILES ${install_files}
             DESTINATION ${install_destination})
+    
+endfunction()
 
-    # If conanfile exists build into local cache
-    if(CONAN_PACKAGE AND EXISTS ${PROJECT_SOURCE_DIR}/conanfile.py)
+function(package_export)
+    _package_check_initialized()
+
+    set(options)
+    set(one_value_args CONAN_PACKAGE)
+    set(multi_value_args)
+    cmake_parse_arguments(args "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+
+    if(CONAN_EXPORT)
         add_custom_target(${args_NAME}_conan_create ALL
-            COMMAND conan create ${PROJECT_SOURCE_DIR} --name=${PROJECT_NAME} --version=${PROJECT_VERSION} --build=missing
+            COMMAND conan create ${PROJECT_SOURCE_DIR} --name=${args_CONAN_PACKAGE} --version=${PROJECT_VERSION} --build=missing
             USES_TERMINAL
             VERBATIM
-            COMMENT "Building and installing ${PROJECT_NAME}/${PROJECT_VERSION} into local cache"
+            COMMENT "Building and installing ${args_CONAN_PACKAGE}/${PROJECT_VERSION} into local cache"
         )
     endif()
-    
 endfunction()
