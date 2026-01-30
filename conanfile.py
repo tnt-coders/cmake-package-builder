5fca857bc6d923588b78f04cffca1de63d25ef39
tjwrona <tjwrona1992@gmail.com>
2026-01-30 00:38:06 -0500

    Removed layout

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMake
 import os
 
 class CMakePackageBuilderConan(ConanFile):
@@ -9,11 +9,7 @@ class CMakePackageBuilderConan(ConanFile):
     url = "https://github.com/tnt-coders/cmake-package-builder"
     description = "CMake utility functions for simplified project setup/installation"
     package_type = "build-scripts"
-    settings = "os", "compiler", "build_type", "arch"
     exports_sources = "CMakeLists.txt", "cmake/*"
-
-    def layout(self):
-        cmake_layout(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
