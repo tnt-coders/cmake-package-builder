fbb805c3b1000f268518075d0d541da73e95c789
tjwrona <tjwrona1992@gmail.com>
2026-02-23 23:43:56 -0500

    corrected conanfile since no cpp dependencies actually exist "none" is supposed to be the right choice here

--- conanfile.py
+++ conanfile.py
@@ -34,5 +34,5 @@ class CMakePackageBuilderConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.builddirs = [os.path.join("lib", "cmake", "PackageBuilder")]
-        self.cpp_info.set_property("cmake_find_mode", "config")
+        self.cpp_info.set_property("cmake_find_mode", "none")
         self.cpp_info.set_property("cmake_file_name", "PackageBuilder")
